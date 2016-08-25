c2c572565c38211be140b0379a8ccb90d3c65c9b
Alexander Kotliarov <slicer256@gmail.com>
2016-08-25 23:01:36 +0300

    properly working timer

--- timer.js
+++ timer.js
@@ -2,9 +2,24 @@
 
 var timer = (function() {
 
-    var timer_display;
+    var display_el;
+    var current_seconds = 5 * 60;
+    var is_active = false;
+
+    var format_time = function(number) {
+        return Math.floor(number/60) + ':' + (number - Math.floor(number/60) * 60)
+            + (number % 10 == 0 ? '0' : '');
+    }
 
     var render = function() {
+
+        display_el.innerHTML = format_time(current_seconds);
+
+        if (is_active) {
+            current_seconds--;
+            window.setTimeout(render, 1000);
+        }
+
         console.log('rendered...');
     };
 
@@ -12,19 +27,29 @@ var timer = (function() {
 
 
         init: function(el) {
-            this.timer_display = el;
+            display_el = el;
+
             console.log('Timer initialized');
         },
 
         start: function() {
+            is_active = true;
+            render();
             console.log('Timer started');
         },
 
         stop: function() {
+            is_active = false;
+            render();
+
             console.log('Timer stopped');
         },
 
         clear: function() {
+            current_seconds = 5 * 60;
+            is_active = false;
+            render();
+
             console.log('Timer resetted');
         }
     }
